340e41f65e3fb1c093297cffc6dbdbafb3bc6ff6
sarazamora <sarazamora510@gmail.com>
2016-07-26 13:51:47 -0700

    Initial Commit

---
+++ Planty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Planty
+//
+//  Created by OG Sara on 7/26/16.
+//  Copyright © 2016 Sprout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
